94bf05c8a064fc4f8fa92dd88544b6d1331f05d2
tmcnichol <tlmcnichol@gmail.com>
2019-01-12 14:56:35 -0700

    Templates for Restaurant actions

--- vagrant/2_project/finalProject.py
+++ vagrant/2_project/finalProject.py
@@ -6,36 +6,38 @@ from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///restaurantmenu.db',
-    connect_args={'check_same_thread': False},
-    poolclass=StaticPool, echo=True)
-Base.metadata.bind = engine
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
 
 #All Restaurants
 @app.route('/restaurants/')
-def restaurantList():
-    return "Return a list of all restaurants and link to menu"
+def showRestaurants():
+    return render_template(
+        'showrestaurants.html', restaurants=restaurants)
 
 
 #New Restaurant
 @app.route('/restaurant/new')
 def newRestaurant():
-    return "Return page to add new Restaurant"
+    return render_template('newrestaurant.html', restaurant=restaurant)
 
 
 #Edit Restaurant
-@app.route('/restaurant/<int:restaurant_id>/edit', methods=['GET', 'POST'])
-def editRestaurant(restaurant_id, menu_id):
-    return "Return a form to POST an EDIT to a Restaurant"
+@app.route('/restaurant/1/edit', methods=['GET', 'POST'])
+def editRestaurant():
+    return render_template('editrestaurant.html', restaurant=restaurant)
 
 
 #Delete Restaurant
-@app.route('/restaurant/<int:restaurant_id>/delete', methods=['GET', 'POST'])
-def deleteRestaurant(restaurant_id, menu_id):
-    return "Return a confirmation page to DELETE a Restaurant"
+@app.route('/restaurant/1/delete', methods=['GET', 'POST'])
+def deleteRestaurant():
+    return render_template('deleterestaurant.html', restaurant=restaurant)
 
 
 #Restaurant Menu
